6bcef618d2ae5ef8675c9be57db219f1a94dce4c
Edwin Munoz <eam20@cobalt.clear.rice.edu>
2024-10-02 23:56:39 -0500

    finished code check 1

--- Main.java
+++ Main.java
@@ -1,22 +1,20 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.lang.Math;
 public class Main {
 
     private static final String[] categories = {"LOAD", "STORE", "LOADI", "ADD", "SUB", "MULT", "LSHIFT", "RSHIFT", "OUTPUT", "NOP", "CONSTANT", "REGISTER", "COMMA", "INTO", "EOF", "EOL"};
     public static int lineNum = 0;
     public static int index;
     public static String line;
-    public static BufferedReader reader;
     public static int flag = -1;
     public static int bCount;
     public static boolean failed;
     public static IR curr;
+    public static int maxR;
     public static void main(String[] args) throws Exception{
-        String[] flags = {"-h", "-r", "-p", "-s"};
-
-        
+        String[] flags = {"-h", "-x", "number"}; 
         String fileName = "";
         boolean flagRepeat = false;
 
@@ -24,24 +22,11 @@ public class Main {
             if (args[i].equals("-h")) {
                 printHelp();
                 return;
-            } else if (args[i].equals("-r")) {
-                if (flag > -1) 
+            } else if (args[i].equals("-x")) {
+                if (flag != -1) 
                     flagRepeat = true;
-                if (flag == -1)
+                else
                     flag = 1;
-                flag = Math.min(flag, 1);
-            } else if (args[i].equals("-p")) {
-                if (flag > -1) 
-                    flagRepeat = true;
-                if (flag == -1)
-                    flag = 2;
-                flag = Math.min(flag, 2);
-            } else if (args[i].equals("-s")) {
-                if (flag > -1) 
-                    flagRepeat = true;
-                if (flag == -1)
-                    flag = 3;
-                flag = Math.min(flag, 3);
             } else {
                 fileName = args[i];
                 break;
@@ -58,29 +43,107 @@ public class Main {
         if (flagRepeat)
             printError("Multiple flags were provided. Please only provide a single flag. Flag -"+ flags[flag] + " was chosen.");
         
-        IR head;
-        if (flag == 1) {
-            head = new IR(0, -1, null);
-            curr = head;
-        }
-        if (flag == 2) {
-            bCount = 0;
-        } 
+        IR head = new IR(0, -1, null);
+        curr = head;
+        bCount = 0;
+        maxR = -1;
+        scan(fileName);
+        if (failed)
+            return;
+        rename();
+        printRenamed();
+        
+    }
 
-        scan(fileName);   
-        if (flag == 2)  {
-            if (failed) {
-                System.out.println("Parse found errors.");
-            } else {
-                System.out.println("Parse succeeded. Processed "+bCount+" operations.");
+    public static void printRenamed() {
+        curr = curr.next;
+        while (curr != null) {
+            if (curr.upcode == 0) {
+                System.out.println(categories[curr.upcode].toLowerCase() + " r" + curr.arguments[0].getVR() + " => r"+ curr.arguments[2].getVR());
+            } else if (curr.upcode == 1) {
+                System.out.println(categories[curr.upcode].toLowerCase() + " r" + curr.arguments[0].getVR() + " => r"+ curr.arguments[1].getVR());
             }
+            else if (curr.upcode == 2) {
+                System.out.println("loadI " + curr.arguments[0].getSR() + " => r"+ curr.arguments[2].getVR());
+            } else if (curr.upcode > 2 && curr.upcode < 8) {
+                System.out.println(categories[curr.upcode].toLowerCase() + " r" + curr.arguments[0].getVR() + ", r" + curr.arguments[1].getVR() +" => r"+ curr.arguments[2].getVR());
+            } else if (curr.upcode == 8) {
+                System.out.println("output " + curr.arguments[0].getSR());
+            } else if (curr.upcode == 9) {
+                System.out.println("nop");
+            }
+            curr = curr.next;
         }
+    }
+
+    public static void rename() {
+        int virtualName = 0;
+        int[] lu = new int[maxR + 1];
+        int[] converter = new int[maxR + 1];
+        for (int i =  0; i <= maxR; i++) {
+            lu[i] = -1;
+            converter[i] = -1;
+        }
+        int ind = bCount;
+        while (curr.lineNum > 0) {
+            // Each definition
+            if(curr.upcode > -1 && curr.upcode < 8 && curr.upcode != 1) {
+                if (converter[curr.arguments[2].getSR()] == -1)
+                    converter[curr.arguments[2].getSR()] = virtualName++;
+                curr.arguments[2].setVR(converter[curr.arguments[2].getSR()]);
+                curr.arguments[2].setNU(lu[curr.arguments[2].getSR()]);
+
+                // Kill live range
+                converter[curr.arguments[2].getSR()] = -1;
+                lu[curr.arguments[2].getSR()] = -1;
+            }
+
+            // Each use 
+            if (curr.upcode == 0) {
+                if (converter[curr.arguments[0].getSR()] == -1)
+                    converter[curr.arguments[0].getSR()] = virtualName++;
+                curr.arguments[0].setVR(converter[curr.arguments[0].getSR()]);
+                curr.arguments[0].setNU(lu[curr.arguments[0].getSR()]);
+                lu[curr.arguments[0].getSR()] = ind;
+            }
+            if (curr.upcode == 1) {
+                if (converter[curr.arguments[1].getSR()] == -1)
+                        converter[curr.arguments[1].getSR()] = virtualName++;
+                curr.arguments[1].setVR(converter[curr.arguments[1].getSR()]);
+                curr.arguments[1].setNU(lu[curr.arguments[1].getSR()]);
+                lu[curr.arguments[1].getSR()] = ind;
+                if (converter[curr.arguments[0].getSR()] == -1)
+                    converter[curr.arguments[0].getSR()] = virtualName++;
+                curr.arguments[0].setVR(converter[curr.arguments[0].getSR()]);
+                curr.arguments[0].setNU(lu[curr.arguments[0].getSR()]);
+                lu[curr.arguments[0].getSR()] = ind;
+            }
+            if(curr.upcode > 2 && curr.upcode < 8 ) {
+                if (converter[curr.arguments[0].getSR()] == -1)
+                    converter[curr.arguments[0].getSR()] = virtualName++;
+                curr.arguments[0].setVR(converter[curr.arguments[0].getSR()]);
+                curr.arguments[0].setNU(lu[curr.arguments[0].getSR()]);
+                lu[curr.arguments[0].getSR()] = ind;
+
+                if (curr.upcode > 2) {
+                    if (converter[curr.arguments[1].getSR()] == -1)
+                        converter[curr.arguments[1].getSR()] = virtualName++;
+                    curr.arguments[1].setVR(converter[curr.arguments[1].getSR()]);
+                    curr.arguments[1].setNU(lu[curr.arguments[1].getSR()]);
+                    lu[curr.arguments[1].getSR()] = ind;
+                }
+            }
+            ind--;
+            curr = curr.prev;
+        }
+
     }
 
     public static void scan(String fileName) {
         index = 0;
         try {
-            reader = new BufferedReader(new FileReader(fileName));
+            
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
             failed = false;
             while ((line = reader.readLine()) != null) {
                 line += "\n";
@@ -94,8 +157,7 @@ public class Main {
                     index = 0;
                     continue;
                 }   
-                System.out.print(line);
-                System.out.println("Line num "+lineNum+" upcode: "+upcode[0]);
+                //System.out.println("Line num "+lineNum+" upcode: "+upcode[0]);
                 if (upcode[0] > -1 && upcode[0] < 2) {
                     finishMemop(upcode[0]);                    
                 } else if (upcode[0] == 2){
@@ -109,9 +171,9 @@ public class Main {
                 } else {
                     printError("Block does not start with an upcode.");
                 }
-                System.out.println();
                 index = 0;
             }
+            reader.close();
         } catch(IOException e) {
             printError("There was an issue trying to read te specified file");
         }    
@@ -168,7 +230,7 @@ public class Main {
                             if (c == 't') {
                                 c = line.charAt(index++);
                                 if (c == ' ' || c == '\t')
-                                    return new int[]{2, 3};
+                                    return new int[]{6, 0};
                             }
                         }
                     }
@@ -298,24 +360,17 @@ public class Main {
     }
 
     public static void finishArith(int operation) {
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: ARITHOP; Lexeme: " + categories[operation]);
-        }
+
         int[] r1 = getNextToken();
         if (r1[0] != 11) {
             printError("Arithmetic operation must be followed by a register.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: REGISTER; Lexeme: r" + r1[1]);
-        }
+        maxR = Math.max(maxR, r1[1]);
 
         if (getNextToken()[0] != 12) {
             printError("Registers 1 must be must be followed by a comma.");
             return ;
-        }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: COMMA; Lexeme: ,");
         }
 
         int[] r2 = getNextToken();
@@ -323,16 +378,11 @@ public class Main {
             printError("Comma must be followed by a second register.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: REGISTER; Lexeme: r" + r2[1]);
-        }
+        maxR = Math.max(maxR, r2[1]);
 
         if (getNextToken()[0] != 13) {
             printError("Registers 2 must be must be followed by a '=>'' .");
             return ;
-        }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: INTO; Lexeme: =>");
         }
 
         int[] r3 = getNextToken();
@@ -340,19 +390,14 @@ public class Main {
             printError("=> must be followed by a third register.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: REGISTER; Lexeme: r" + r3[1]);
-        }
+        maxR = Math.max(maxR, r3[1]);
 
         if (getNextToken()[0] != 15) {
             printError("Too many tokens given for operation.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: EOL; Lexeme: \\n");
-        }
 
-        if (flag == 1 && failed == false) {
+        if (failed == false) {
             Argument[] arguments = new Argument[3];
             Argument arg1 = new Argument(r1[1], 0, 0, 0);
             Argument arg2 = new Argument(r2[1], 0, 0, 0);
@@ -360,41 +405,29 @@ public class Main {
             arguments[0] =arg1;
             arguments[1] =arg2;
             arguments[2] =arg3;
-            System.out.println("Line: "+lineNum+"; Opcode: "+categories[operation]+"; SR1: "+r1[1]+", VR1: -"+", PR1: -"+", NU1: -"
-            +"; SR2: "+r2[1]+", VR2: -"+", PR2: -"+", NU2: -"
-            +"; SR3: "+r3[1]+", VR3: -"+", PR3: -"+", NU3: -; \n");
             curr.next = new IR(lineNum, operation, arguments);
+            curr.next.prev = curr;
             curr = curr.next;
-
+            bCount++;
         }
 
-        if (flag == 2)
-            bCount++;
+            
         
         
 
     } 
     
     public static void finishMemop(int operation) {
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: MEMOP; Lexeme: " + categories[operation]);
-        }
         int[] r1 = getNextToken();
-        
         if (r1[0] != 11) {
             printError("Arithmetic operation must be followed by a register.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: REGISTER; Lexeme: r" + r1[1]);
-        }
+        maxR = Math.max(maxR, r1[1]);
 
         if (getNextToken()[0] != 13) {
             printError("Register 1 must be must be followed by a '=>'' .");
             return ;
-        }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: INTO; Lexeme: =>");
         }
 
         int[] r2 = getNextToken();
@@ -402,55 +435,40 @@ public class Main {
             printError("=> must be followed by a second register.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: REGISTER; Lexeme: r" + r2[1]);
-        }
+        maxR = Math.max(maxR, r2[1]);
 
         if (getNextToken()[0] != 15) {
             printError("Too many tokens given for operation.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: EOL; Lexeme: \\n");
-        }
 
-        if (flag == 1 && failed == false) {
+        if (failed == false) {
             Argument[] arguments = new Argument[3];
             Argument arg1 = new Argument(r1[1], 0, 0, 0);
             Argument arg2 = new Argument(r2[1], 0, 0, 0);
             arguments[0] =arg1;
-            arguments[1] =arg2;
-            System.out.println("Line: "+lineNum+"; Opcode: "+categories[operation]+"; SR1: "+r1[1]+", VR1: -"+", PR1: -"+", NU1: -"
-            +"; SR2: "+r2[1]+", VR2: -"+", PR2: -"+", NU2: -; \n");
+            if (operation == 0)
+                arguments[2] =arg2;
+            else
+                arguments[1] =arg2;
             curr.next = new IR(lineNum, operation, arguments);
+            curr.next.prev = curr;
             curr = curr.next;
-
+            bCount++;
         }
 
-        if (flag == 2)
-            bCount++;
+            
     }
     
     public static void finishLoadI() {
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: MEMOP; Lexeme: " + categories[2]);
-        }
-
         int[] r1 = getNextToken();
         if (r1[0] != 10) {
             printError("LoadI operation must be followed by a constant.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: CONSTANT; Lexeme: "+ r1[1]);
-        }
-
         if (getNextToken()[0] != 13) {
             printError("Register 1 must be must be followed by a '=>'' .");
             return ;
-        }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: INTO; Lexeme: =>");
         }
 
         int[] r2 = getNextToken();
@@ -458,91 +476,66 @@ public class Main {
             printError("=> must be followed by a register.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: REGISTER; Lexeme: r" + r2[1]);
-        }
+        maxR = Math.max(maxR, r2[1]);
 
         if (getNextToken()[0] != 15) {
             printError("Too many tokens given for operation.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: EOL; Lexeme: \\n");
-        }
 
-        if (flag == 1 && failed == false) {
+        if (failed == false) {
             Argument[] arguments = new Argument[3];
             Argument arg1 = new Argument(r1[1], 0, 0, 0);
             Argument arg2 = new Argument(r2[1], 0, 0, 0);
             arguments[0] =arg1;
-            arguments[1] =arg2;
-            System.out.println("Line: "+lineNum+"; Opcode: "+categories[2]+"; SR1: "+r1[1]+", VR1: -"+", PR1: -"+", NU1: -"
-            +"; SR2: "+r2[1]+", VR2: -"+", PR2: -"+", NU2: -; \n");
+            arguments[2] =arg2;
             curr.next = new IR(lineNum, 2, arguments);
+            curr.next.prev = curr;
             curr = curr.next;
+            bCount++;
         }
 
-        if (flag == 2)
-            bCount++;
+            
     }
 
     public static void finishOutput() {
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: OUTPUT; Lexeme: " + categories[8]);
-        }
-        
         int[] r1 = getNextToken();
         if (r1[0] != 10) {
             printError("Output operation must be followed by a constant.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: CONSTANT; Lexeme: "+ r1[1]);
-        }
 
         if (getNextToken()[0] != 15) {
             printError("Too many tokens given for operation.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: EOL; Lexeme: \\n");
-        }
 
-        if (flag == 1 && failed == false) {
+        if (failed == false) {
             Argument[] arguments = new Argument[3];
             Argument arg1 = new Argument(r1[1], 0, 0, 0);
             arguments[0] =arg1;
-            System.out.println("Line: "+lineNum+"; Opcode: "+categories[8]+"; SR1: "+r1[1]+", VR1: -"+", PR1: -"+", NU1: -; \n");
-            curr.next = new IR(lineNum, 2, arguments);
+            curr.next = new IR(lineNum, 8, arguments);
+            curr.next.prev = curr;
             curr = curr.next;
-        }
-
-        if (flag == 2)
             bCount++;
+        }
+            
     }
 
     public static void finishNop() {
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: NOP; Lexeme: " + categories[9]);
-        }
-
         if (getNextToken()[0] != 15) {
             printError("Too many tokens given for operation.");
             return ;
         }
-        if (flag == 3) {
-            System.out.println("Line: " + lineNum + "; Type: EOL; Lexeme: \\n");
-        }
-
-        if (flag == 1 && failed == false) {
-            Argument[] arguments = new Argument[3];
-            System.out.println("Line: "+lineNum+"; Opcode: "+categories[8]+" \n");
-            curr.next = new IR(lineNum, 2, arguments);
+        
+        if (failed == false) {
+            curr.next = new IR(lineNum, 9, null);
+            curr.next.prev = curr;
             curr = curr.next;
+            bCount++;
         }
 
-        if (flag == 2)
-            bCount++;
+            
     }
     
     public static void printHelp() {
@@ -568,6 +561,8 @@ class IR {
         this.lineNum = lineNum;
         this.upcode = upcode;
         this.arguments = args;
+        prev = null;
+        next = null;
     }
 } 
 
@@ -593,6 +588,12 @@ class Argument {
     }
     public int getNU() {
         return this.NU;
+    }
+    public void setVR(int newVr) {
+        this.VR = newVr;
+    }
+    public void setNU(int newNU) {
+        this.NU = newNU;
     }
 }
 
